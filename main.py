d6a9da1c90678cfe79aac7605fe28e8c17c3b8f0
Steve Scott <stevescott517@gmail.com>
2025-01-28 20:23:45 -0500

    pydrived replaced wth googleapiclient and google_auth_oauthlib

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 
 ## import libraries
 import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 from dotenv import  load_dotenv
 from pathlib import Path
-
+import logging
 ## load the environment variables
 load_dotenv(f"C:\\Users\\{os.getlogin()}\\secrets\.dotenv")
 ### connect to googe drive
-gauth = GoogleAuth()
-gauth.LoadClientConfigFile(f"C:\\Users\\{os.getlogin()}\\secrets\\client_secrets.json")
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
 
-### List the files in the MyDrive/LoseIt directory
-file_list = drive.ListFile({'q': "'LoseIt' in parents and trashed=false"}).GetList()
-print(file_list)
+flow = InstalledAppFlow.from_client_secrets_file(
+    f"""c:\\users\\{os.getlogin()}\\secrets\\client_secrets.json""",
+    scopes= "https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email"
+)
+flow.run_local_server()
+credentials = flow.credentials
+'''
+auth_uri = flow.authorization_url()
+code = input("Enter the authorization code")
+flow.fetch_token(code=code)
+'''
+service = build('drive', 'v3', credentials=credentials)
+user_info_service = build('oauth2', 'v2', credentials=credentials)
+user_info = user_info_service.userinfo().get().execute()
+print(user_info['email'])
+service.close()
+logging.info('service is closed')
